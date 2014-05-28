6019eb0e2dcde1f597fa01b546b632691eb7a37a
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-05-28 16:50:58 -0400

    add base project

---
+++ WWDCBadges/WWDCBadges/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  WWDCBadges
+//
+//  Created by Chris Gonzales on 5/28/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        
+    }
+    return 0;
+}
+
