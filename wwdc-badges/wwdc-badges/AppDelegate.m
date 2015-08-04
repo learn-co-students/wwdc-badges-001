568c98aed56388213de507b0971def0e6ee33ef3
vicfriedman <victoria.m.friedman@gmail.com>
2015-08-04 14:17:41 -0400

    finished

--- wwdc-badges/wwdc-badges/AppDelegate.m
+++ wwdc-badges/wwdc-badges/AppDelegate.m
@@ -21,6 +21,14 @@
     // Hey!
     
     // Write your code here!!
+    NSArray *firstName = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    
+    NSArray *lastName = @[@"Borg", @"Kay", @"Lovelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    
+    for (NSUInteger i = 0; i < [firstName count]; i++){
+        NSLog(@"Hello, my name is %@ %@.", firstName[i], lastName[i]);
+    }
+    
     
     return YES;
 }
