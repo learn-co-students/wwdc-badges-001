460c050b9bc721b5e7c0fddad65e4e39d9c8dcd3
markedwardmurray <markedwardmurray@gmail.com>
2015-09-14 13:07:30 -0400

    insert solution

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -18,6 +18,44 @@
      
      */
     
+    NSArray *conferenceSpeakers = @[ @"Anita Borg",
+                                     @"Alan Kay",
+                                     @"Ada Lovelace",
+                                     @"Aaron Swartz",
+                                     @"Alan Turing",
+                                     @"Michael Faraday",
+                                     @"Grace Hopper",
+                                     @"Charles Babbage" ];
+    
+    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+    }
+    
+    // Advanced
+    /*
+    NSArray *firstNames = @[ @"Anita",
+                             @"Alan",
+                             @"Ada",
+                             @"Aaron",
+                             @"Alan",
+                             @"Michael",
+                             @"Grace",
+                             @"Charles" ];
+    
+    NSArray *lastNames = @[ @"Borg",
+                            @"Kay",
+                            @"Lovelace",
+                            @"Swartz",
+                            @"Turing",
+                            @"Faraday",
+                            @"Hopper",
+                            @"Babbage"  ];
+    
+    for (NSUInteger i = 0; i < [firstNames count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", firstNames[i], lastNames[i]);
+    }
+     */
+    
     return YES;
 }
 
