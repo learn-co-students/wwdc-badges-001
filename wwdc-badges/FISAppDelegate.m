aebf4e198e7aa305d95a3c8dec60e272801f67df
Diego-F-Aguirre <diego3884@gmail.com>
2015-10-21 10:35:39 -0400

    wwdc-badges complete

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    
+    NSArray *firstNames = @[@"Anita",@"Alan",@"Ada",@"Aaron",@"Alan",@"Michael",@"Grace",@"Charles"];
+    NSArray *lastNames = @[@"Borg",@"Kay",@"Lovelace",@"Swartz",@"Turing",@"Faraday",@"Hopper",@"Babbage"];
+    
+    for (NSUInteger i = 0; i < [firstNames count]; i++) {
+        NSLog(@"Hello, my name is %@ %@.", firstNames[i], lastNames[i]);
+    }
     
     // do not alter
     return YES;  //
