a3d4589550b4d533b65913212eb8386ccddfc7c7
pwjablonski <pwjablonski@gmail.com>
2016-04-08 22:18:20 -0400

    Done.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,13 @@
      
      */
     
+    NSArray *conferenceSpeakers = @[@"Anitia Borg", @"Alan Kay", @" Ada Lovelace", @"Aaron Swarta", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage" ];
+    
+    for(NSUInteger i = 0; i < conferenceSpeakers.count; i++ ){
+        
+        NSLog(@"Hello, my name is %@", conferenceSpeakers[i]);
+    }
+    
     // do not alter
     return YES;  //
     ///////////////
